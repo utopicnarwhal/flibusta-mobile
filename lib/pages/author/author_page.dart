87a9bff9e39ba980dc2b3e89ae05f15947e9a757
Sergei <gigok@bk.ru>
2020-03-22 22:17:55 +0300

    commit

--- lib/pages/author/author_page.dart
+++ lib/pages/author/author_page.dart
@@ -1,82 +1,84 @@
-// import 'package:dio/dio.dart';
-// import 'package:flibusta/ds_controls/ui/app_bar.dart';
-// import 'package:flibusta/model/authorInfo.dart';
-// import 'package:flibusta/services/http_client.dart';
-// import 'package:flibusta/utils/html_parsers.dart';
-// import 'package:utopic_toast/utopic_toast.dart';
-// import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flibusta/ds_controls/ui/app_bar.dart';
+import 'package:flibusta/model/authorInfo.dart';
+import 'package:flibusta/services/http_client.dart';
+import 'package:flibusta/utils/html_parsers.dart';
+import 'package:utopic_toast/utopic_toast.dart';
+import 'package:flutter/material.dart';
 
-// class AuthorPage extends StatefulWidget {
-//   static const routeName = "/AuthorPage";
+class AuthorPage extends StatefulWidget {
+  static const routeName = "/AuthorPage";
 
-//   final int authorId;
+  final int authorId;
 
-//   const AuthorPage({Key key, this.authorId}) : super(key: key);
-//   @override
-//   _AuthorPageState createState() => _AuthorPageState();
-// }
+  const AuthorPage({Key key, this.authorId}) : super(key: key);
+  @override
+  _AuthorPageState createState() => _AuthorPageState();
+}
 
-// class _AuthorPageState extends State<AuthorPage> {
-//   Dio _dio = ProxyHttpClient().getDio();
-//   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+class _AuthorPageState extends State<AuthorPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-//   AuthorInfo authorInfo;
+  AuthorInfo authorInfo;
 
-//   @override
-//   void initState() {
-//     super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-//     getAuthorInfo(widget.authorId).then((response) {
-//       setState(() {
-//         authorInfo = response;
-//       });
-//     });
-//   }
+    getAuthorInfo(widget.authorId).then((response) {
+      setState(() {
+        authorInfo = response;
+      });
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       key: _scaffoldKey,
-//       appBar: DsAppBar(
-//         title: Text(authorInfo.name ?? 'Загрузка...'),
-//       ),
-//       body: GridCards(
-//         scaffoldKey: _scaffoldKey,
-//         data: authorInfo.books,
-//       ),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: DsAppBar(
+        title: Text(authorInfo.name ?? 'Загрузка...'),
+      ),
+      // body: GridDataScreen(
+      //   scaffoldKey: _scaffoldKey,
+      //   data: authorInfo.books,
+      // ),
+    );
+  }
 
-//   Future<AuthorInfo> getAuthorInfo(int authorId) async {
-//     authorInfo = AuthorInfo(id: authorId);
-//     try {
-//       Uri url = Uri.https(ProxyHttpClient().getHostAddress(),
-//           "/a/" + authorId.toString());
-//       var response = await _dio.getUri(url);
+  Future<AuthorInfo> getAuthorInfo(int authorId) async {
+    authorInfo = AuthorInfo(id: authorId);
+    try {
+      Uri url = Uri.https(
+        ProxyHttpClient().getHostAddress(),
+        "/a/" + authorId.toString(),
+      );
 
-//       authorInfo = parseHtmlFromAuthorInfo(response.data, authorId);
-//     } catch (e) {
-//       ToastManager().showToast(
-//         'Не удалось получить данные об авторе',
-//         action: SnackBarAction(
-//           label: "Попробовать ещё раз",
-//           onPressed: () {
-//             getAuthorInfo(widget.authorId).then((response) {
-//               setState(() {
-//                 authorInfo = response;
-//               });
-//             });
-//           },
-//         ),
-//       );
-//       print(e);
-//     }
+      var response = await ProxyHttpClient().getDio().getUri(url);
 
-//     return authorInfo;
-//   }
+      authorInfo = parseHtmlFromAuthorInfo(response.data, authorId);
+    } catch (e) {
+      ToastManager().showToast(
+        'Не удалось получить данные об авторе',
+        action: ToastAction(
+          label: "Попробовать ещё раз",
+          onPressed: (_) {
+            getAuthorInfo(widget.authorId).then((response) {
+              setState(() {
+                authorInfo = response;
+              });
+            });
+          },
+        ),
+      );
+      print(e);
+    }
 
-//   @override
-//   void dispose() {
-//     super.dispose();
-//   }
-// }
+    return authorInfo;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
